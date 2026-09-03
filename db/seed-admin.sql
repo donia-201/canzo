32ba309defa046544ee83f169847fd482c80e179
Donia Afify <doniaafify615@gmail.com>
2026-09-03 17:54:30 +0300

    new admin added

--- db/seed-admin.sql
+++ db/seed-admin.sql
@@ -1,4 +1,4 @@
 INSERT OR IGNORE INTO users (email, user_name, user_role, password_hash) VALUES
   ('moaz@gmail.com', 'Moaz', 'Admin', '$2b$10$NveMeyKNb6eGsho2xGNrd.1vzeU90SZ7CRmSFeUyxFWA9sXrFLXCW');
 INSERT OR IGNORE INTO users (email, user_name, user_role, password_hash) VALUES
-  ('md@gmail.com', 'Md', 'Admin', '$2b$10$VLPaigMLmIqvc/NgXpybB.ZdH0/7Dbgv0qE21TbdTyhzEIPQ6pJa6');
+  ('md@gmail.com', 'Moaz', 'Admin', '$2b$10$VLPaigMLmIqvc/NgXpybB.ZdH0/7Dbgv0qE21TbdTyhzEIPQ6pJa6');
