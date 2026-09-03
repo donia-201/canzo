b5be380b12ddfbd75699b2459b3a123373b03b6e
Donia Afify <doniaafify615@gmail.com>
2026-09-03 17:37:28 +0300

    new Admin Added

--- db/seed-pricing.sql
+++ db/seed-pricing.sql
@@ -1,9 +1,10 @@
 INSERT OR IGNORE INTO pricing (material, activity_type, price_per_kg) VALUES
-  ('Plastic', 'Wedding hall', 25),  // water pricing has changed from 12 to 25
-  ('Canz', 'Wedding hall', 18),
-  ('Plastic', 'Cafe', 10),
-  ('Canz', 'Cafe', 12.5),
-  ('Plastic', 'Club', 11),
-  ('Canz', 'Club', 16),
-  ('Plastic', 'Restaurant', 10),
-  ('Canz', 'Restaurant', 17);
+  ('Plastic', 'Wedding hall', 25),   
+  -- water pricing has changed from 12 to 25
+  ('Canz', 'Wedding hall', 250),
+  -- ('Plastic', 'Cafe', 10),
+  ('Canz', 'Cafe', 200),
+  -- ('Plastic', 'Club', 11),
+  ('Canz', 'Club', 200),
+  -- ('Plastic', 'Restaurant', 10),
+  ('Canz', 'Restaurant', 200);
